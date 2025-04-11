2f52eb0f80369522130f2a828341a57fce47485d
alisa <mamonova.ag@phystech.edu>
2025-04-11 20:22:25 +0300

    quiz changes

--- DODbot/database.py
+++ DODbot/database.py
@@ -6,7 +6,6 @@ DATABASE = "base.db"
 
 def get_connection():
     conn = sqlite3.connect("base.db", check_same_thread=False)
-    conn.execute("PRAGMA journal_mode=WAL")
     if not hasattr(conn, 'is_closed') or conn.is_closed:
         conn = sqlite3.connect(...)
     return conn
