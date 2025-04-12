24423371797430d607850c9da46b3bd4e3acdf6a
alisa <mamonova.ag@phystech.edu>
2025-04-12 21:56:18 +0300

    aiogram

--- DODbot/main.py
+++ DODbot/main.py
@@ -9,12 +9,10 @@ import users
 import admin
 import handlers
 import admin_handlers
-import newsletter
 import add_admin
 import merch
 import quiz
 import set_points
-
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -33,11 +31,13 @@ def index():
 @app.route("/your-webhook-path", methods=["POST"])
 async def webhook():
     try:
+        # Flask request.get_data() работает синхронно,
+        # поэтому можно использовать его напрямую в async view
         data = request.get_data().decode("utf-8")
         update = types.Update.model_validate_json(data)
         await dp.feed_update(bot, update)
     except Exception as e:
-        logging.exception("Ошибка при обработке обновления:")
+        logger.exception("Ошибка при обработке обновления:")
         return "Ошибка", 500
     return "OK", 200
 
@@ -57,7 +57,6 @@ async def on_shutdown(dispatcher):
     await bot.delete_webhook()
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
     loop = asyncio.get_event_loop()
     loop.run_until_complete(on_startup())
     app.run(host="0.0.0.0", port=10181)
