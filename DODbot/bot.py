ce6a747464e8a0c3d17e7db844dd666096a2c560
alisa <mamonova.ag@phystech.edu>
2025-04-12 20:40:51 +0300

    aiogram

--- DODbot/bot.py
+++ DODbot/bot.py
@@ -1,7 +1,9 @@
 from aiogram import Bot, Dispatcher, Router
+from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from config import TOKEN
 
-bot = Bot(token=TOKEN, parse_mode=ParseMode.HTML)
+bot = Bot(token=TOKEN, default=DefaultBotProperties(
+    parse_mode=ParseMode.HTML))
 dp = Dispatcher()
 router = Router()
