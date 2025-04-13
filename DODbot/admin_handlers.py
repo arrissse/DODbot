b4028a0744574e17d55eb03ce3f9ca74fd092c06
alisa <mamonova.ag@phystech.edu>
2025-04-13 05:47:30 +0300

    give merch

--- DODbot/admin_handlers.py
+++ DODbot/admin_handlers.py
@@ -388,7 +388,7 @@ async def process_fusername(m: Message, state: FSMContext):
                 price = await get_merch_price(merch)
                 markup.button(InlineKeyboardButton(
                     f"{merch}: {price}", callback_data=f'give_merch:{price}:{merch}:{username}'))
-        if markup:
+        if markup.as_markup().inline_keyboard:
             await m.answer(f"Количество баллов {username}: {await check_points(username)}. Выберите мерч пользователю {username}:", reply_markup=markup)
         else:
             await m.answer(f"❌ Пользователь {username} не может получить мерч. Количество баллов: {await check_points(username)}")
